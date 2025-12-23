393fc4d39c236d617ca38f53386fe5d5d325e34b
Miqayelgit <User@LAPTOP-RHPCQ4O2>
2025-12-23 22:52:52 +0400

    Home commit

--- Program.cs
+++ Program.cs
@@ -339,9 +339,164 @@
         }
         return count;
     }
-    public static void Main() 
+
+    public static float ThreeMultAVG_238(int[] arr)
     {
-        int[] arr = { 6, 8, 9, 3, 10, 0, 0 };
-        Console.WriteLine(ZeroValueCount_237(arr));
+        int count = 0;
+        float sum = 0;
+
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 3 == 0)
+            {
+                sum += arr[i];
+                count++;
+            }
+        }
+
+        return sum / count;
+    }
+
+    public static float SevenMultCount_240(int[] arr)
+    {
+        int count = 0;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 7 == 0)
+                count++;
+        }
+
+        return count;
+    }
+
+    public static int SumElementsMultToK_241(int[] arr, int k)
+    {
+        int sum = 0;
+        for(int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % k == 0)
+            { 
+                sum += arr[i];
+            }
+        }
+
+        return sum;
+    }
+    public static int MultElementsMultToM_242(int[] arr, int m)
+    {
+        int mult = 1;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % m == 0)
+            {
+                mult *= arr[i];
+            }
+        }
+
+        return mult;
+    }
+
+    public static int CountElementsMultToT_243(int[] arr, int t)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % t == 0)
+                count++;
+        }
+
+        return count;
+    }
+
+    public static int MultOfNumsWith2Remainder_244(int[] arr)
+    {
+
+        int mult = 1;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 5 == 2)
+            {
+                mult *= arr[i];
+            }
+        }
+
+        return mult;
+    }
+
+    public static int SumPowOfNumsWithNonRemainder_245(int[] arr)
+    {
+        int sum = 0;
+        for(int i = 0;i < arr.Length;i++)
+        {
+            if ((arr[i] + i) % 3 == 0)
+            {
+                sum += arr[i] * arr[i];
+            }
+        }
+
+        return sum;
+    }
+
+    public static int AvgOfElWithIndexPow_246(int[] arr)
+    {
+        int count = 0;
+        int sum = 0;
+        for(int i = 1; i < arr.Length; i++)
+        {
+         //TODO add validation part
+            if(i < 5)
+            {
+                count++;
+                sum += arr[i];
+            }
+        }
+        return sum / count;
+    }
+
+    public static int SumOfElIndexPow_248(int[] arr, int k)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int powOfsumOfElAndIndex = (arr[i] + i) * (arr[i] + i);
+
+            if (powOfsumOfElAndIndex % k == 0)
+            {
+                sum += arr[i];
+            }
+        }
+
+        return sum;
+    }
+
+    public static int CountOfElValAndIndex_249(int[] arr, int k)
+    {
+        int count = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int subtract = arr[i] - i;
+            subtract = subtract < 0 ? subtract * -1 : subtract;
+
+            if (subtract > k)
+                count++;
+        }
+
+        return count;
+    }
+
+    public static int PowMult_250(int[] arr)
+    {
+        int mult = 1;
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            int multOfValAndIndex = arr[i] * i;
+            if(multOfValAndIndex % 3 == 2)
+            {
+                mult *= arr[i] * arr[i];
+            }
+        }
+        return mult;
     }
 }
